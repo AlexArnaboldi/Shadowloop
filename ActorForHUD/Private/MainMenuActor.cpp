707744c39b45376556f0d2449146d2cb462f984a
AlexArnaboldi <alex.arnaboldi97@hotmail.com>
2025-05-22 17:35:54 +0200

    Add files via upload

---
+++ ActorForHUD/Private/MainMenuActor.cpp
@@ -0,0 +1,97 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MainMenuActor.h"
+
+#include "HUD/SLButtonContinue_M.h"
+#include "HUD/SLButtonExit_M.h"
+#include "HUD/SLButtonPlay_M.h"
+#include "HUD/SLHUDMenu.h"
+#include "HUD/SLHUDWidget.h"
+
+AMainMenuActor::AMainMenuActor(): Book()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void AMainMenuActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	StartPullLoc = GetActorLocation();
+	
+}
+
+USLHUDWidget* AMainMenuActor::CastToHUDWidget()
+{
+	APlayerController* PlayerController = GetWorld()->GetFirstPlayerController();
+	if (!IsValid(PlayerController)) { return nullptr; }
+
+	AHUD* HUD = PlayerController->GetHUD();
+	if (!IsValid(HUD)) { return nullptr; }
+
+	ASLHUDMenu* HUDMenuRef = Cast<ASLHUDMenu>(HUD);
+	if (!IsValid(HUDMenuRef)) { return nullptr; }
+
+	USLHUDWidget* HUDWidgetRef = HUDMenuRef->GetHUDWidget();
+
+	HUDWidget = HUDWidgetRef;
+	if (!HUDWidget.IsValid())
+	{
+		UE_LOG(LogTemp, Error, TEXT("HUDWidget is invalid"));
+		return nullptr;
+	}
+	
+	UE_LOG(LogTemp, Display, TEXT("HUDWidget is valid"));
+	
+	return HUDWidgetRef;
+}
+
+void AMainMenuActor::CastToButton()
+{
+	switch (Book)
+	{
+		case EWhichBook::NewGame:
+			NewGame = HUDWidget->GetButtonStartLevel();;
+			if (NewGame.IsValid())
+			{
+				NewGame->OnPullBookNewGame.AddDynamic(this, &AMainMenuActor::Pull);
+				NewGame->OnPushBookNewGame.AddDynamic(this, &AMainMenuActor::Push);
+			}
+			else
+			{
+				UE_LOG(LogTemp, Error, TEXT("AMainMenuActor::ButtonGame is invalid"));
+			}
+			//UE_LOG(LogTemp, Warning, TEXT("AMainMenuActor::New Game"));
+		break;
+		
+		case EWhichBook::Continue:
+			Continue = HUDWidget->GetButtonContinue();
+			if (Continue != nullptr)
+			{
+				Continue->OnPullBookContinue.BindUFunction(this, TEXT("Pull"));
+				Continue->OnPushBookContinue.AddDynamic(this, &AMainMenuActor::Push);
+			}
+			else
+			{
+				UE_LOG(LogTemp, Warning, TEXT("AMainMenuActor::Continue is invalid"))
+			}
+			//UE_LOG(LogTemp, Warning, TEXT("AMainMenuActor::Continue"));
+		break;
+
+		case EWhichBook::Quit:
+			USLButtonExit_M* Quit = HUDWidget->GetButtonQuit();
+			if (Quit != nullptr)
+			{
+				Quit->OnPullQuit.BindUFunction(this, TEXT("Pull"));
+				Quit->OnPushQuit.BindUFunction(this, TEXT("Push"));
+				Quit->OnQuit.BindUFunction(this, TEXT("Quit"));
+			}
+			else
+			{
+				UE_LOG(LogTemp, Warning, TEXT("AMainMenuActor::Quit is invalid"))
+			}
+		break;
+	}
+}
+
---
+++ ActorForHUD/Public/MainMenuActor.h
@@ -0,0 +1,82 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MainMenuActor.generated.h"
+
+class USLButtonContinue_M;
+class USLButtonPlay_M;
+class USLHUDWidget;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnNewGameButton, float, ProgressBarNewGame);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnContinueButton, float, ProgressBarContinue);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnQuitButton);
+
+UENUM(BlueprintType)
+enum class EWhichBook : uint8
+{
+	NewGame UMETA(DisplayName = "New Game"),
+	Continue UMETA(DisplayName = "Continue"),
+	Quit UMETA(DisplayName = "Quit")
+};
+
+UCLASS()
+class SHADOWLOOP_API AMainMenuActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AMainMenuActor();
+
+// ------------------------ Delegate
+	UPROPERTY(BlueprintCallable, BlueprintType)
+	FOnNewGameButton OnNewGameButton;
+
+	UPROPERTY(BlueprintCallable, BlueprintType)
+	FOnContinueButton OnContinueButton;
+
+	UPROPERTY(BlueprintCallable, BlueprintType)
+	FOnQuitButton OnQuitButton;
+
+// ------------------------ Event to call bind
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent)
+	void Pull();
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent)
+	void Push();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void Quit();
+	
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FVector StartPullLoc = FVector::ZeroVector;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FVector FinalPullLoc = FVector::ZeroVector;
+
+	UPROPERTY(BlueprintReadWrite)
+	float ProgressBar = 0.0f;
+
+	UPROPERTY()
+	TWeakObjectPtr<USLHUDWidget> HUDWidget = nullptr;
+	
+	UPROPERTY()
+	TWeakObjectPtr<USLButtonPlay_M> NewGame = nullptr;
+
+	UPROPERTY()
+	TWeakObjectPtr<USLButtonContinue_M> Continue = nullptr;
+
+	UPROPERTY(EditAnywhere, Category = "enum | Button")
+	EWhichBook Book;
+
+	UFUNCTION(BlueprintCallable)
+	USLHUDWidget* CastToHUDWidget();
+
+	UFUNCTION(BlueprintCallable)
+	void CastToButton();
+};
